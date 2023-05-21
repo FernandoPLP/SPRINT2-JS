5897598bd9e27e09d95fb5118dbc40d76658caad
RodriguesEric134 <RM550249@fiap.com.br>
2023-05-21 17:12:38 -0300

    criado variável para usuário

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const usuario = {
+    nome: document.querySelector('#c-nome'),
+    sobrenome: document.querySelector('#c-sobrenome'),
+    email: document.querySelector('#c-email'),
+    senha: document.querySelector('#c-senha'),
+    confirmasenha: document.querySelector('#c-confirmasenha'),
+  };
